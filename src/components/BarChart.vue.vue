c75dc56ba6fd6901de0d9fbd7e039c59ecaf2419
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-03 18:41:15 +0900

    Home画面を修正

--- src/components/BarChart.vue.vue
+++ src/components/BarChart.vue.vue
@@ -3,25 +3,27 @@ import { Bar } from "vue-chartjs";
 export default {
   extends: Bar,
   data() {
+    const labelsArray = [
+      "8/25",
+      "8/26",
+      "8/27",
+      "8/28",
+      "8/29",
+      "8/30",
+      "8/31",
+      "9/1",
+      "9/2",
+      "9/3",
+    ];
+    const dataArray = [40, 30, 50, 25, 70, 40, 30, 50, 25, 70];
     return {
       chartdata: {
-        labels: [
-          "8/25",
-          "8/26",
-          "8/27",
-          "8/28",
-          "8/29",
-          "8/30",
-          "8/31",
-          "9/1",
-          "9/2",
-          "9/3",
-        ],
+        labels: labelsArray,
         datasets: [
           {
             label: ["摂取カロリー"],
             backgroundColor: "#f87979",
-            data: [40, 30, 50, 25, 70, 40, 30, 50, 25, 70],
+            data: dataArray,
           },
         ],
       },
