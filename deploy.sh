0435b9fc6d48badd85b1ab4d81eb1bd327f4bfa5
tomosue <nikoniko04211970@gmail.com>
2021-09-03 14:53:47 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+​
 # 名前のデータ取る
 name=$(cat yourname.env | sed s/YOURNAME=//)
-
+​
 writename(){
     if [ "$(ls lambda/ | grep "${name}")" ]; then
         echo "既にあなたの名前に変わっているため、変更は行いません"
@@ -16,11 +16,11 @@ writename(){
         cd ..
     fi
 }
-
+​
 lambda_deploy(){
-
+​
     cd lambda
-
+​
     for lambdaf in *.js
     do
         funame=$(echo $lambdaf | sed s/.js//)
@@ -29,13 +29,13 @@ lambda_deploy(){
         aws lambda create-function  --function-name ${funame} \
         --runtime nodejs14.x \
         --role arn:aws:iam::347867041416:role/internship_exec_role \
-        --handler ${fname}.handler  --zip-file fileb://${funame}.zip \
+        --handler ${funame}.handler  --zip-file fileb://${funame}.zip \
         --region ap-northeast-1
         rm ${funame}.zip
     done
     cd ..
 }
-
+​
 lambda_update(){
     cd lambda
     for updatelist in *.js
@@ -48,16 +48,16 @@ lambda_update(){
     done
     cd ..
 }
-
+​
 s3_deploy(){
     aws s3 mb s3://${name}-internship
 }
-
+​
 s3_update(){
     npm run build
     aws s3 cp ./dist s3://${name}-internship --recursive --acl public-read
 }
-
+​
 deletename(){
     cd lambda
     for noname in *.js
@@ -68,7 +68,7 @@ deletename(){
     echo "名前を削除しました"
     cd ..
 }
-
+​
 case $1 in
     "") writename;;
     lambda_deploy) lambda_deploy;;
