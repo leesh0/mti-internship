9b42b101f66bf6bd3c9f1aadceb57b8878ce5118
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-03 15:45:41 +0900

    ページを作成

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,34 +1,53 @@
 <template>
   <div class="ui secondary pointing green inverted massive menu">
-    <a class="item" @click="switchPage('Home')" v-bind:class="{active: current=='home'}">Home</a>
-    <a class="item" @click="switchPage('User')" v-bind:class="{active: current=='user'}">Users</a>
-    <a class="item" @click="switchPage('Profile')" v-bind:class="{active: current=='profile'}">Profile</a>
+    <a
+      class="item"
+      @click="switchPage('Home')"
+      v-bind:class="{ active: current == 'home' }"
+      >Home</a
+    >
+    <a
+      class="item"
+      @click="switchPage('Exercise')"
+      v-bind:class="{ active: current == 'exercise' }"
+      >Exercise</a
+    >
+    <a
+      class="item"
+      @click="switchPage('Food')"
+      v-bind:class="{ active: current == 'food' }"
+      >Food</a
+    >
+    <a
+      class="item"
+      @click="switchPage('Profile')"
+      v-bind:class="{ active: current == 'profile' }"
+      >Profile</a
+    >
     <div class="right menu">
       <button class="item" @click="logout">Logout</button>
     </div>
   </div>
 </template>
 <script>
-  export default {
-    name: 'Menu',
-    props: {
-      current: {
-        type: String,
-        required: true,
-      },
+export default {
+  name: "Menu",
+  props: {
+    current: {
+      type: String,
+      required: true,
     },
-    methods: {
-      switchPage(pageName) {
-        this.$router.push({name: pageName});
-      },
-      logout() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        this.$router.push({name: 'Login'});
-      },
+  },
+  methods: {
+    switchPage(pageName) {
+      this.$router.push({ name: pageName });
     },
-  }
+    logout() {
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+      this.$router.push({ name: "Login" });
+    },
+  },
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
