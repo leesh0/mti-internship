e4ee62feed1e243c17546b5dce38520efe585aff
tomosue <nikoniko04211970@gmail.com>
2021-09-03 17:49:32 +0900

    baseUrlを変更

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://aj4d9wdj65.execute-api.ap-northeast-1.amazonaws.com/dev";
+export const baseUrl = "https://l6ychw38u6.execute-api.ap-northeast-1.amazonaws.com";
 // 自分のエンドポイントに置き換えてください
