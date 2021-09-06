77fa2a57dc5389f59dd02ff7444cba45a2aa02d6
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-06 12:25:50 +0900

    Exerciseページを編集

--- src/views/Exercise.vue
+++ src/views/Exercise.vue
@@ -4,72 +4,82 @@
     <div class="ui main container">
       <!-- 基本的なコンテンツはここに記載する -->
       <div class="ui segment">
-        <form class="ui form">
-          <div class="field">
-            <label>ユーザ名</label>
-            <input type="text" placeholder="Nickname" v-model="nickname" />
-          </div>
-          <div class="field">
-            <label>年齢</label>
-            <div class="inline fields">
-              <div class="field">
-                <input type="number" v-model="start" />
-                <label>歳から</label>
-              </div>
-              <div class="field">
-                <input type="number" v-model="end" />
-                <label>歳まで</label>
+        <h2>カロリー摂取・消費状況</h2>
+        <Chart />
+        <div class="ui message">
+          <div class="header">アドバイス</div>
+          <div class="ui comments">
+            <div class="comment">
+              <a class="avatar">
+                <img
+                  src="https://semantic-ui.com/images/avatar/small/stevie.jpg"
+                />
+              </a>
+              <div class="content">
+                <a class="author">アドバイザー</a>
+                <div class="text">
+                  カロリー摂取が安定していません。安定した食生活にしましょう。
+                </div>
               </div>
             </div>
           </div>
-        </form>
+        </div>
       </div>
-      <div class="ui segments">
-        <template v-for="(user, index) in filterdUsers">
-          <div class="ui segment wrapper" :key="index">
-            <h2 class="ui header">
-              <div class="content">
-                {{ user.nickname }}
-                <div class="ui green label">
-                  {{ user.age }}
+      <div class="ui segment">
+        <h2>おすすめの運動</h2>
+        <ul>
+          <li>
+            <h3>ウォーキング 10分</h3>
+          </li>
+          <li>
+            <h3>スクワット 20回</h3>
+          </li>
+        </ul>
+      </div>
+      <div class="ui segment">
+        <h2>運動の記録</h2>
+        <div class="field">
+          <form class="ui form">
+            <div class="field">
+              <label>行った運動を選択してください。</label>
+            </div>
+            <div class="ui grid">
+              <div class="four wide column">
+                <div class="ui card">
+                  <div class="content">
+                    <h2 class="ui header">
+                      <div class="sub header">
+                        筋トレ
+                      </div>
+                      腕立て
+                      <div class="sub header">
+                        カロリー : 132kcal 回数: 15 × 3
+                      </div>
+                    </h2>
+                    <button class="fluid ui green button">check</button>
+                  </div>
                 </div>
               </div>
-              <div class="sub header">
-                {{ user.userId }}
-              </div>
-            </h2>
-          </div>
-        </template>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-    <Footer></Footer>
   </div>
 </template>
 <script>
 import Menu from "@/components/Menu.vue";
-import Footer from "@/components/Footer.vue";
 import { baseUrl } from "@/assets/config.js";
 import axios from "axios";
-
+import Chart from "@/components/Chart.vue";
 export default {
   name: "Exercise",
   components: {
     Menu,
-    Footer,
+    Chart,
   },
   data() {
-    return {
-      // Vue.jsで使う変数はここに記述する
-      users: [],
-      nickname: null,
-      start: 0,
-      end: 100,
-      query: {
-        nickname: null,
-        start: null,
-        end: null,
-      },
-    };
+    return {};
   },
   computed: {
     // 計算した結果を変数として利用したいときはここに記述する
@@ -101,7 +111,6 @@ export default {
     // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
     const token = localStorage.getItem("token");
     if (!token) this.$router.push({ name: "Login" });
-
     axios
       .get(baseUrl + "/users")
       .then((response) => {
