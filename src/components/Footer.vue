74398dd5cb0242ac8c9ac6b910f9b5c91aab4dcb
tomosue <nikoniko04211970@gmail.com>
2021-09-07 11:53:51 +0900

    Footerを中央に表示

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="ui inverted vertical footer segment">
       <div class="ui container">
-        サニタス 2021. All Rights Reserved
+        <div class="ui center aligned grid">
+            サニタス 2021. All Rights Reserved
+        </div>
       </div>
     </div>
 </template>
 <style scoped>
 .footer{
+  padding-top: 25px;
   position: absolute;/*←絶対位置*/
   bottom: 0; /*下に固定*/
   width: 100%;
